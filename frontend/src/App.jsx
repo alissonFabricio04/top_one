edae1d2822bbef9f1572421af64e6996ef39e373
alissonFabricio04 <alissonfabricio04@gmail.com>
2022-11-22 16:00:29 -0300

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
+import { Toast, ToastHeader, ToastBody } from "reactstrap";
 import axios from "axios";
+// import { FcClock, FcBusinessman, FcConferenceCall, FcServices, FcCalendar } from "react-icons/fc";
 import Navegação from "./components/Navigation";
 
 export const API_URL = "http://localhost:8083";
@@ -13,11 +15,31 @@ function App() {
       .catch(e => console.log(e))
   }, [])
 
+  const verifyHour = (hour) => {
+    const date = new Date().toLocaleTimeString('pt-PT', { hour12: false })
+    return date <= hour ? true : false
+  };
+
   return(
     <div>
       <Navegação />
-      <input type="text" placeholder="teste" className="form-control" />
-      {atividades.map(atividade => <li>{ atividade.idActivitySession }</li>)}
+      <div style={{ display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center", flexWrap: "wrap" }}>
+          {
+            atividades.map(atividade => {
+              return verifyHour(atividade.startTime) ?
+                (
+                  <Toast className="mt-6 m-4" id={atividade.idActivitySession}>
+                    <ToastHeader>{atividade.area} {atividade.startTime} - {atividade.endTime}</ToastHeader>
+                    <ToastBody>
+                      <h4 style={{ fontSize: "1rem" }}>{atividade.name}</h4>
+                      <p>{atividade.client ? atividade.client : "Cliente não cadastrado"}</p>
+                      <p>{atividade.instructor}</p>
+                    </ToastBody>
+                  </Toast>
+                ) : null
+            })
+          }
+      </div>
     </div>
   )
 }
--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import logo from "./img/logo.png"
 
-
+// #82e13a
 
 function Navegação() {
   return (
     <div>
-      <nav class="navbar navbar-expand-lg navbar-dark" style={{background: "#FFDEAD"}}>
+      <nav class="navbar navbar-expand-lg navbar-dark" style={{background: "#70c232"}}>
         <img src={logo} alt="" width={"70"} />
       </nav>
     </div>
